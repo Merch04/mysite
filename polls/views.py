a73b209af1e893e2bf032d39ac7500c2aa9f2a79
DanilAnk <ankdanil2003@gmail.com>
2022-05-13 17:17:58 +0300

    master

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,49 @@
+from ast import Try
 from django.http import HttpResponse
 from django.shortcuts import render
+import sqlite3
+
+
+from datetime import datetime
+#import pytz
+
+
+
+def read_sqlite_table(start_rows, end_rows):
+    try:
+        sqlite_connection = sqlite3.connect('db-controller.sqlite')
+        cursor = sqlite_connection.cursor()
+        print("Подключен к SQLite")
+
+        sqlite_select_query = f"SELECT * from telemetry where time BETWEEN {start_rows} AND {end_rows}"
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchall()
+        print("Всего строк:  ", len(records))
+        print("Вывод каждой строки")
+        need_times = []
+        for i in range(1, len(records)):
+            last_row = records[i-1]
+            real_row = records[i]
+            if real_row[1] - last_row[1] >= 2:
+                last_time = datetime.fromtimestamp(last_row[1])
+                real_time = datetime.fromtimestamp(real_row[1])
+                need_times.append([last_row, real_row])
+                print(f"{last_time.strftime('%Y-%m-%d %H:%M:%S')}  ->  {real_time.strftime('%Y-%m-%d %H:%M:%S')}")
+
+        cursor.close()
+        return need_times
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+        return None
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
+
 
 
 def index(request):
+  need_times = read_sqlite_table(1649774679, 1649774762)
+
   return render(request, 'polls/html/index.html')
