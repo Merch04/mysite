2dad5acd67f1ab4df12108a97e7e74abb921594c
RetrasTT <retrast.keke3h@gmail.com>
2022-05-13 11:55:58 +0300

    master

--- authorization/views.py
+++ authorization/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'templates/authorization/index.html')
+    return render(request, 'authorization/index.html')
