bebc17b88a765258d361d4b787a9aa7c094763fc
Re_Grishka <n29d02@gmail.com>
2022-05-13 17:22:42 +0700

    s

---
+++ test.py
@@ -0,0 +1,10 @@
+import matplotlib.path as mplPath
+import numpy as np
+
+a = [[0,832],[516,35],[904,37],[1486,710], [1482,830]]
+bbPath = mplPath.Path(np.array([[a[0], a[1]],
+                     [a[1], a[2]],
+                     [a[2], a[3]],
+                     [a[3], a[0]]]))
+
+bbPath.contains_point((200, 100))
