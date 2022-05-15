ac5dd21463163193e6304cfad3a6806eccf2791b
RetrasTT <99965026+RetrasTT@users.noreply.github.com>
2022-05-15 16:43:07 +0300

    master

--- polls/views.py
+++ polls/views.py
@@ -29,9 +29,10 @@ def read_sqlite_table(times, shift):
             id=shift[0])[0].videos[0]['start'])
 
         for i in range(2, len(object_arr)):
+            y = object_arr[i-1].detectionCoordinates["y1"]
             last_row = object_arr[i-1]
             real_row = object_arr[i]
-            if real_row.time - last_row.time >= 60:
+            if real_row.time - last_row.time >= 60 and y <= 720:
                 last_time = datetime.fromtimestamp(last_row.time)
                 real_time = datetime.fromtimestamp(real_row.time)
 
@@ -55,6 +56,121 @@ def read_sqlite_table(times, shift):
     except sqlite3.Error as error:
         print("Ошибка при работе с SQLite", error)
         return None
+
+
+# ---------------------------------------------GOVNO----NO rabotaet------------------------------------------
+
+
+def Customer_counts(times, shift):
+    clients = []
+    First = True
+    ErrorTime = 0
+    Loop = False
+    ErrorCounter = 20
+    TimeToClient = 5
+    TimeCustomer = {
+        'StartTime': 0,
+        'EndTime': 0,
+        'SumTime': 0
+    }
+    dt = datetime.strptime(times[0], "%Y-%m-%d %H:%M:%S")
+    start_time = datetime.timestamp(dt) + 25200
+    dt = datetime.strptime(times[1], "%Y-%m-%d %H:%M:%S")
+    end_time = datetime.timestamp(dt) + 25200
+
+    for x in Telemetry.objects.filter(time__gt=start_time, time__lt=end_time, shiftId=shift[0]):
+
+        y1 = x.detectionCoordinates['y1']
+        y2 = x.detectionCoordinates['y2']
+
+        center = (y1+y2)/2
+
+        if (center <= 130):
+            # Точка входа в последовательность клиентных координат
+            if (First):
+                TimeCustomer = {
+                    'StartTime': x.time,
+                    'EndTime': 0,
+                    'SumTime': 1
+                }
+                Loop = True
+                First = False
+                continue
+            else:
+                # Если следующая строка не первое вхождение, и является клиентом, добавляем время, учитывая разрывы
+                TimeCustomer['SumTime'] = abs(
+                    TimeCustomer['StartTime'] - x.time)
+        else:
+            # пока количество допустимых ошибок меньше 10 продолжаем работу
+            if (ErrorTime <= ErrorCounter and Loop):
+                ErrorTime += 1
+                continue
+
+            # Если Ошибок уже много для допущения и время свыше 60 секунд, отправляем клиента в БД
+            if (ErrorTime >= ErrorCounter and TimeCustomer['SumTime'] > TimeToClient and Loop):
+                TimeCustomer['EndTime'] = TimeCustomer['StartTime'] + \
+                    TimeCustomer['SumTime']
+                clients.append(TimeCustomer)
+                ErrorTime = 0
+                First = True
+                Loop = False
+
+            # Если ошибок достаточно много и не набралось нужного количества координат от клиента, то нахуй его такого клиента, заебали блять ходят нахуй трясутся суки ебаые прячутся от камеры за ебаным продавцом меня заебало эту хуйню уже писать я сам не понимаю что я пишу когда уже кто нибудь придет и решит все мои проблемы пырнув меня ножом в подворотне что бы не идти на ебучую сессию по истории а.......
+            if (ErrorTime >= ErrorCounter and TimeCustomer['SumTime'] < TimeToClient and Loop):
+                ErrorTime = 0
+                First = True
+                Loop = False
+            # А если совем все плохо и у нас мусорные данные, просто игнорим их
+            else:
+                ErrorTime = 0
+                First = True
+    clients = Unite(clients, 145)
+    return clients
+
+
+# ----------------------------------------------------------
+def Unite(array, acc):
+    output = []
+    Buf = {
+        'StartTime': -1,
+        'EndTime': -1,
+        'SumTime': -1
+    }
+
+    First = True
+    i = 0
+    while i < len(array):
+        if First:
+            Buf = {'StartTime': array[i]['StartTime'], 'EndTime': array[i]
+                   ['EndTime'], 'SumTime': array[i]['SumTime']}
+            First = False
+        # Если пересекаются, то объединяем
+        elif ((Buf['EndTime'] + acc >= array[i]['StartTime'] or Buf['StartTime'] >= array[i]['EndTime'])):
+            Buf = {'StartTime': Buf['StartTime'], 'EndTime': array[i]
+                   ['EndTime'], 'SumTime': array[i]['EndTime']-Buf['StartTime']}
+        else:
+            output.append(Buf)
+            First = True
+            i -= 1
+        i += 1
+
+    # Костыль ибо хз как отлавливать последний элемент да и мне лень че та
+    if ((output[-1]['EndTime'] + acc >= array[-1]['StartTime'] or output[-1]['StartTime'] >= array[-1]['EndTime'])):
+        Buf = {'StartTime': output[-1]['StartTime'], 'EndTime': array[-1]
+               ['EndTime'], 'SumTime': array[-1]['EndTime']-output[-1]['StartTime']}
+        output.append(Buf)
+    else:
+        output.append(array[-1])
+
+    return output
+# ----------------------------------------------------------
+
+
+def average_time_customers(customers):
+    average_time = 0
+    average_time = sum([i['SumTime'] for i in customers])
+    average_time /= len(customers)
+    return time.strftime("%H:%M:%S", time.gmtime(average_time))
 
 
 def index(request):
@@ -86,14 +202,18 @@ def statics(request):
     else:
         times = request.session.get('times', None)
         shift_id = request.session.get('shiftId', None)
-
+        customers = Customer_counts(times, shift_id)
         miss_time, miss_timecode, temp_samp = read_sqlite_table(
             times, shift_id)
+        average_time = average_time_customers(customers)
         video = Video.objects.filter(shiftId=shift_id[0])
+        print(len(customers), average_time)
         content = {
             'miss_time': miss_time,
             'miss_timecode': miss_timecode,
             'temp_samp': temp_samp,
+            'customers': len(customers),
+            'average_time_customers': average_time,
             "video": video,
         }
         return render(request, 'polls/statics.html', content)
