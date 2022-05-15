731cde269263d4142d48b23b280f903d2341b06a
Re_Grishka <n29d02@gmail.com>
2022-05-15 22:30:41 +0700

    master

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', include('polls.urls'), name="period"),
     path('admin/', admin.site.urls),
     path('', include('authorization.urls')),
+    path('/login', LogoutView.as_view(template_name="index.html"), name="logout"),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('statics/', views.statics, name="statics"),
     
+    path('', views.index, name="period"),
+    path('statics/', views.statics, name="statics"),
     path('ajax/load-shifts/', views.load_shifts, name='ajax_load_shifts'), # AJAX
 ]
